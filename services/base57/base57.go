33545fb9594e94dc312d2fd42806d96ee40337d6
Maxxxxxx-x <maxxng1028@gmail.com>
2025-06-09 15:41:36 +0800

    feat: Base57 encoding, decoding and generation functions

---
+++ services/base57/base57.go
@@ -0,0 +1,66 @@
+package base57
+
+import (
+	"crypto/rand"
+	"errors"
+	"math/big"
+)
+
+const charset = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789"
+
+var (
+	encodeMap = []rune(charset)
+	decodeMap = make(map[rune]int)
+	base      = big.NewInt(int64(len(charset)))
+)
+
+func init() {
+	for i, c := range charset {
+		decodeMap[c] = i
+	}
+}
+
+func Encode(input []byte) string {
+	num := new(big.Int).SetBytes(input)
+	if num.Sign() == 0 {
+		return string(encodeMap[0])
+	}
+
+	var result []rune
+	mod := new(big.Int)
+	for num.Sign() > 0 {
+		num.DivMod(num, base, mod)
+		result = append([]rune{encodeMap[mod.Int64()]}, result...)
+	}
+
+	return string(result)
+}
+
+func Decode(encoded string) (string, error) {
+	num := big.NewInt(0)
+
+	for _, c := range encoded {
+		val, ok := decodeMap[c]
+		if !ok {
+			return "", errors.New("invalid character in input: " + string(c))
+		}
+		num.Mul(num, base)
+		num.Add(num, big.NewInt(int64(val)))
+	}
+
+	return string(num.Bytes()), nil
+}
+
+func GenerateRandom(length int) (string, error) {
+	result := make([]byte, length)
+	max := big.NewInt(int64(len(charset)))
+
+	for i := range length {
+		num, err := rand.Int(rand.Reader, max)
+		if err != nil {
+			return "", err
+		}
+		result[i] = charset[num.Int64()]
+	}
+	return string(result), nil
+}
